370fa9e5adb80fd1ac34fecfa9f5e056704cd3e8
shivansh193 <shivanshkalra796@gmail.com>
2023-04-14 03:25:12 +0530

    Added opt for a service page, a bit of work needed

---
+++ src/pages/bookAServicePage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from "./components/navbar"
+import FormCompiled from "./components/formCompiled.js"
+import Form from "./components/form"
+const App = () => {
+  return (
+    <div>
+      {/* <Navbar /> */}
+
+      <FormCompiled />
+    </div>
+  )
+}
+
+export default App
